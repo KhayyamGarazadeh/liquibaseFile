27a79ed10b5143dc18484449f8d222018211feb1
KhayyamGarazadeh <xeyyamqarazade123@gmail.com>
2023-07-16 23:33:35 +0400

    Liquibase task

---
+++ src/main/java/com/example/liquibasefile/LiquibaseFileApplication.java
@@ -0,0 +1,13 @@
+package com.example.liquibasefile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquibaseFileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiquibaseFileApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/liquibasefile/dao/ProductEntity.java
@@ -0,0 +1,2 @@
+package com.example.liquibasefile.dao;public class ProductEntity {
+}
