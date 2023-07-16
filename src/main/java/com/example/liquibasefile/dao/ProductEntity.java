f0e5e767a2c22a6a8137aaf32778d8fbd6dd66c5
KhayyamGarazadeh <xeyyamqarazade123@gmail.com>
2023-07-16 23:37:47 +0400

    Liquibase task

--- src/main/java/com/example/liquibasefile/dao/ProductEntity.java
+++ src/main/java/com/example/liquibasefile/dao/ProductEntity.java
@@ -1,2 +1,26 @@
-package com.example.liquibasefile.dao;public class ProductEntity {
+package com.example.liquibasefile.dao;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Table(name="products")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class ProductEntity {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    private String name;
+    private Double price;
+    private String description;
+
 }
